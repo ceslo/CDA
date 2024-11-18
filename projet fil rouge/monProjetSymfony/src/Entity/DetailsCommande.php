788bbe41ff5bc8be73ae96f4ee8a1ae830560514
ceslo <sloves.ce@gmail.com>
2024-11-18 14:52:37 +0100

    TEST RETOUR EN ARRIERE

--- projet fil rouge/monProjetSymfony/src/Entity/DetailsCommande.php
+++ projet fil rouge/monProjetSymfony/src/Entity/DetailsCommande.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\DetailsCommandeRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: DetailsCommandeRepository::class)]
+#[ApiResource]
 class DetailsCommande
 {
     #[ORM\Id]
